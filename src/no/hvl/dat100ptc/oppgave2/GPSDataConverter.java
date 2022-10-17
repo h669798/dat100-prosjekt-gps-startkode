d70ba90b486d30d1ac902c60d134d24b744d3a0b
Elena N.J. <elena.nicole.jacobsen@hotmail.com>
2022-10-17 09:19:03 +0200

    Oppgave 2 ferdig (oppdatert).

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -3,11 +3,6 @@ package no.hvl.dat100ptc.oppgave2;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class GPSDataConverter {
-
-	// konverter tidsinformasjon i gps data punkt til antall sekunder fra midnatt
-	// dvs. ignorer information om dato og omregn tidspunkt til sekunder
-	// Eksempel - tidsinformasjon (som String): 2017-08-13T08:52:26.000Z
-      // skal omregnes til sekunder (som int): 8 * 60 * 60 + 52 * 60 + 26 
 	
 	private static int TIME_STARTINDEX = 11;
 
@@ -30,9 +25,9 @@ public class GPSDataConverter {
 		GPSPoint gpspoint;
 
 		int time = toSeconds(timeStr);
-		double latitude = toSeconds(latitudeStr);
-		double longitude = toSeconds(longitudeStr);
-		double elevation = toSeconds(elevationStr);
+		double latitude = Double.parseDouble(latitudeStr);
+		double longitude = Double.parseDouble(longitudeStr);
+		double elevation = Double.parseDouble(elevationStr);
 		
 		gpspoint = new GPSPoint(time, latitude, longitude, elevation);
 	      return gpspoint;
